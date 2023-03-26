d1b2d83c6b3cc76d077e439bf33974ab85db5d13
Khthonian <anansicorp@tuta.io>
2023-03-26 02:30:11 +0100

    Corrections.

--- CMP3752M.cpp
+++ CMP3752M.cpp
@@ -11,8 +11,6 @@ Description
 - The contents of this code contain adaptations and improvements of Tutorial 2 and Tutorial 3, for the base code and the kernel functions. This can be found at https://github.com/wing8/OpenCL-Tutorials
 - There is also a kernel function that has been adapted from https://github.com/spoolean/HistogramEqualisation
 - The images that the code was tested on include .ppm and .pgm images. These images can be found in the relevant directories.
-- The intensity histogram implementations feature a serial implementation and a parallel reduction implementation.
-- The cumulative histogram implementations feature a simple implementation, two variations of the Hillis-Steele pattern, and a single implementation of the Blelloch pattern.
 - The user is able to give their own desired bin count, which can affect the output of the image and the histograms produced.
 - The user is also able to select the functions being used.
 - Performance metrics and the histograms are displayed to the user via the console.
